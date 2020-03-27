18c45ddcb5a60d38ad5d2da22013c47f4f4d564e
Julian Antonio Smith <juliansmith94@hotmail.com>
2020-03-27 23:05:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,8 +1,19 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+
+# Write a method that, given an NDS creates a new Hash
+# The return value should be like:
+#
+# { directorOne => allTheMoneyTheyMade, ... }
+
+def directors_totals(nds)
+  result = {}
+  nil
+  directors =[{:name "Paul", :revenue => 123456789}, {:name "Jean" :revenue => 1234567789}, {:name "Sam" :revenue =>1234567889}]
+end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
+
 def gross_for_director(director_data)
   director_index = 0
   totals = {}
@@ -21,17 +32,4 @@ def gross_for_director(director_data)
   end
 
   totals
-end
-end	
-
-# Write a method that, given an NDS creates a new Hash
-# The return value should be like:
-#
-# { directorOne => allTheMoneyTheyMade, ... }
-
-
-def directors_totals(nds)
-result = {}
-  nil
-  directors =[{:name "Paul", :revenue => 123456789}, {:name "Jean" :revenue => 1234567789}, {:name "Sam" :revenue =>1234567889}]
 end
